47b61d0be6bc154edb7d9efece0b4d8e0511254c
devanshg03 <105848104+devanshg03@users.noreply.github.com>
2023-02-06 12:40:25 +0800

    optimized links for SEO

--- components/Projectitem.jsx
+++ components/Projectitem.jsx
@@ -15,7 +15,7 @@ const Projectitem = ({ title, backgroundImg, projectUrl, year }) => {
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">{year}</p>
-        <Link href={projectUrl}>
+        <Link href={projectUrl} aria-label={title}>
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More info
           </p>
