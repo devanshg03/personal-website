88badc49ea59b1444102654c9623db2170639ac0
devanshg03 <105848104+devanshg03@users.noreply.github.com>
2023-02-10 04:22:09 +0800

    added IIMT 3626 page

---
+++ pages/iimt3626.js
@@ -0,0 +1,254 @@
+import Link from "next/link";
+import React from "react";
+
+const iimt3626 = () => {
+  return (
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="mt-20 max-w-[960px] m-auto md:grid grid-cols-1 gap-8">
+        <div className=" col-span-1">
+          <p className="uppercase text-xl tracking-widest text-[#2E77B2]">
+            Podcast
+          </p>
+          <h2 className="py-4">What Drives Who You Are Podcast Interview</h2>
+          <iframe
+            src="https://anchor.fm/devansh-gandhi/embed/episodes/What-Drives-Who-You-Are-Exploring-Values-With-Devansh-e1umngd"
+            height="auto"
+            width="100%"
+            frameborder="0"
+            scrolling="no"
+          ></iframe>
+        </div>
+        <div className="col-span-1">
+          <p className="uppercase text-xl tracking-widest text-[#2E77B2]">
+            Essay
+          </p>
+          <h2 className="py-4">My 3 Core Driving Values</h2>
+          <p className="py-2 text-gray-600">
+            The core values of a person are traits or qualities that represent
+            the highest priorities of a person, and they are deeply held
+            beliefs, and the core, fundamental driving force of a person. As
+            such, these core values are what shape an individual’s personality
+            and how a person interacts with the world. There are a plethora of
+            personality tests and frameworks online, but as being an ESTP, an
+            entrepreneur according to 16 personalities, means that I like to
+            interpret things my own way, and that is the same for my core
+            values. Instead of having goal-driven values such as ‘Achievement’
+            or ‘Wealth’, what drives me is not the end-result but rather the
+            journey itself.
+          </p>
+          <p className="py-2 text-gray-600">
+            Being an ESTP suggests that I am “savvy, energetic and perceptive,
+            and truly enjoy living on the edge”, which relates closely with my
+            top 3 core values of “Vision & Ambition”, “Continuous Learning” and
+            “Creativity”.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            I have always enjoyed living life on the edge, making some of my
+            life's biggest decisions so for on a whim based solely on my
+            intuition. The reasoning behind living on the edge is that I see no
+            downside to this since if they decision works out fine, I will have
+            no regrets, but it does not, I have an opportunity to learn what I
+            did wrong and how I can improve. A recent example of this is my
+            decision to pursue a BBA at the University of Hong Kong. In
+            secondary school, I studied Biology, Chemistry and Information and
+            Communication Technology, thinking that I wanted to go into CS,
+            Bioinformatics or even Biotech. However, the last academic year of
+            secondary school was 2020-2021, where the market went through one of
+            the quickest crashes and recoveries, followed by a market boom.
+            Having my last year of high school during the pandemic allowed me to
+            have a more flexible schedule, and more time to explore my
+            interests. During this time, instead of focusing on my education
+            like everyone else, I spent my time learning about finance, stock
+            trading and how companies work. However, when it came time to write
+            a personal statement for uni, I wrote one up about pursing a degree
+            in Computer Science since I had not changed my intent for my career.
+            However, at the last moment, I decided to change my program rankings
+            and pursue a degree in finance at HKU, with a personal essay about
+            Computer Science. I remember my teachers telling me that I should
+            pick something related to what I studied in secondary, as it would
+            be a safer option, or that I should apply for programs with lower
+            entry requirements. However, I decided to stick with my intuition
+            and go for a degree in Finance at HKU.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            Those that know me well know that I am always working on some
+            passion project, whether it be a YouTube channel between the ages of
+            11 and 16, or a custom phone case startup (‘CAESD’) during the
+            summer after I graduated, or Hong Kong Student Ventures, a
+            consulting project am working on, Nola foodtech/consumer tech
+            startup or even in student clubs like Entrepreneur Society here at
+            HKU. I have always jumped into projects immediately if they have
+            aligned with my vision, and purpose.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            I believe my top value is vision, and it goes hand-in-hand with
+            another value, ambition. The Barret Values Center categorizes
+            ambition and vision as separate values. However, I would classify
+            these as one, since it is my ambition that supports my vision. I
+            believe you be ambitious without a vision, but not have a vision
+            without ambition. I have been fortunate to have great parents that
+            have supported me in all my endeavors, not telling me to stop making
+            YouTube videos every day after school or to focus on getting
+            internships rather than working on startups, my parents have
+            continuously supported me to grow and become independent. I think
+            this ultimately has contributed to how my values are.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            I think the reason having a vision and ambition is my top value is
+            because I try to find meaning and purpose in what I do. One of the
+            high points driven by this value of vision and ambition was when I
+            won a startup competition, I remember pitching our idea to the
+            judges with my team, knowing that we had been working around the
+            clock to have as much of the vision presented as possible, that when
+            I was presenting my goal was not to win competition, but instead to
+            make them believe in my vision, how we were going to change the
+            snack and health industry. It was a formal environment, and I don't
+            really like wearing suits, but I think that moment was a high point
+            where I was really doing what I believed in. I think this really
+            shows that the value of vision and ambition goes deeper than just
+            ‘achievement’ according to the Shwart’z value complex, but rather
+            having the value of vision and ambition is a way for me to follow
+            the underlying desire to make an impact on the world, and help
+            improve people’s lives through my skills.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            My second top value is continuous learning. I have always been
+            interested in the most random topics. I vividly remember having a
+            'Space' book when I was young, and I would read through it learning
+            about the most random facts about planets. I think this is partly
+            because of the values my parents instilled in me when I was young,
+            where they would tell me about different countries in the world, and
+            to learn more about the world, and I believe this value also goes
+            together with my personality of being entrepreneurial. Since to be
+            good leaders we have to be willing to learn and make mistakes.
+            Throughout my various ventures, I have learned lots of skills, from
+            shooting videos, editing audio and video as well as making graphics
+            for my YouTube channel at a young age, to learning to make websites
+            during ICT in secondary school. Our final project was supposed to be
+            a website, where we could even use no-code tools such as Google
+            Sites or Wix.com to make a website, but I wanted to do something
+            unique and unconventional, so instead I learned to code HTML and
+            CSS, and make a cool website from scratch. Those basic skills have
+            been really useful in my development in programming websites and web
+            apps, allowing me to test my entrepreneurial ideas really quick by
+            building functional prototypes to test my ideas out. Furthermore,
+            this value is part of what drives my spontaneous decision-making
+            part of my personality, where I was not afraid to make the last
+            minute decision to pursue a degree in finance because I knew I was
+            willing to be at a disadvantage and learn the basics of economics
+            and finance from scratch. Furthermore, as a leader, continuous
+            learning enables me to support and motivate my team in times of
+            uncertainty, and learn to take up missing roles in a team setting.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            My third top value from the Barret Values Center assessment would be
+            entrepreneurial, but I disagree that it is a value, instead I think
+            being entrepreneurial is a personality, it is driven my core values
+            of vision & ambition as well as continuous learning. As such, I
+            would say that my 3rd value would be creativity. I believe my value
+            of creativity has shaped me to be the person I am for good or for
+            worse, since I find repetitive tasks boring, and always try to find
+            creative ways to automate or improve on my workflow. I believe
+            creativity comes in all shapes and forms, and I have not been the
+            best at drawing or painting like other artistic people. Instead, I
+            found my value of creativity in my unconventional solutions to
+            problems. An example is when
+          </p>
+
+          <p className="py-2 text-gray-600">
+            Overall, my values tend to be spread unevenly in the stages of
+            Psychological Development for the Barret Values Center Assessment,
+            as the section on conformity is empty, and the majority of my values
+            concentrate on individuating and self-actualizing. According to the
+            Schwartz’s Value Circumplex, my values tend to relate closely in the
+            areas of self-enhancement and openness to change rather than
+            self-transcendence or conservation. This is why I think in the long
+            term I would want to work more towards the values of security and
+            benevolence, which are usually weaknesses of an ESTP. However, I
+            believe the values of benevolence or even universalism will help me
+            empathize with people’s problems and enable me to be a better
+            entrepreneur.
+          </p>
+
+          <p className="py-2 text-gray-600">
+            Overall, my values tend to be spread unevenly in the stages of
+            Psychological Development for the Barret Values Center Assessment,
+            as the section on conformity is empty, and the majority of my values
+            concentrate on individuating and self-actualizing. According to the
+            Schwartz’s Value Circumplex, my values tend to relate closely in the
+            areas of self-enhancement and openness to change rather than
+            self-transcendence or conservation. This is why I think in the long
+            term I would want to work more towards the values of security and
+            benevolence, which are usually weaknesses of an ESTP. However, I
+            believe the values of benevolence or even universalism will help me
+            empathize with people’s problems and enable me to be a better
+            entrepreneur.
+          </p>
+
+          <h3 className="text-xl py-2">References</h3>
+
+          <p className="text-gray-600 py-2">
+            Barrett Values Centre, LLC. (2022, December 20). The Barrett Model.
+            Barrett Values Centre.
+            <cite className="pl-2">
+              <a
+                className="text-[#2E77B2] underline cursor-pointer"
+                href="https://www.valuescentre.com/barrett-model/"
+              >
+                https://www.valuescentre.com/barrett-model/
+              </a>
+            </cite>
+          </p>
+
+          <p className="text-gray-600 py-2">
+            Evans, B. B. A. D. (2022, July 21). This happiness hack from a
+            wildly popular Stanford class can help you create a life you love.
+            Quartz.
+            <cite className="pl-2">
+              <a
+                className="text-[#2E77B2] underline cursor-pointer"
+                href="https://qz.com/784279/this-happiness-hack-from-a-wildly-popular-stanford-class-can-help-you-create-a-life-you-love"
+              >
+                https://qz.com/784279/this-happiness-hack-from-a-wildly-popular-stanford-class-can-help-you-create-a-life-you-love
+              </a>
+            </cite>
+          </p>
+
+          <p className="text-gray-600 py-2">
+            NERIS Analytics Limited. (2022, December 1). Entrepreneur (ESTP)
+            Personality. 16Personalities.
+            <cite className="pl-2">
+              <a
+                className="text-[#2E77B2] underline cursor-pointer"
+                href="https://www.16personalities.com/estp-personality"
+              >
+                https://www.16personalities.com/estp-personality
+              </a>
+            </cite>
+          </p>
+
+          <p className="text-gray-600 mb-20 py-2">
+            Schwartz, S. H. (2012). An Overview of the Schwartz Theory of Basic
+            Values. Online Readings in Psychology and Culture, 2(1).
+            <cite className="pl-2">
+              <a
+                className="text-[#2E77B2] underline cursor-pointer"
+                href="https://doi.org/10.9707/2307-0919.1116"
+              >
+                https://doi.org/10.9707/2307-0919.1116
+              </a>
+            </cite>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default iimt3626;
